77bdebd0166e93118145d012470a4bd4a030290d
glhr <grenblot@gmail.com>
2020-03-16 13:53:24 +0100

    cleanup & added spacy tokenizer

--- test_scripts/comparing_tokenizers.py
+++ test_scripts/comparing_tokenizers.py
@@ -2,12 +2,9 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import logging
-import rasa.utils.common
-import rasa.cli.utils as cli_utils
 import rasa.utils.io as io_utils
 
 logger = logging.getLogger(__name__)
-
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 logging.getLogger("absl").setLevel(logging.ERROR)
 logging.getLogger("transformers").setLevel(logging.ERROR)
@@ -20,7 +17,10 @@ from rasa.nlu.tokenizers.mitie_tokenizer import MitieTokenizer
 from rasa.nlu.tokenizers.convert_tokenizer import ConveRTTokenizer
 from rasa.nlu.tokenizers.lm_tokenizer import LanguageModelTokenizer
 from rasa.nlu.utils.hugging_face.hf_transformers import HFTransformersNLP
+
 from rasa.nlu.utils.spacy_utils import SpacyNLP
+from rasa.nlu.config import RasaNLUModelConfig
+from rasa.nlu.components import ComponentBuilder
 
 from rasa.nlu.training_data import Message
 
@@ -37,17 +37,18 @@ test_input = "Okay, pick up this yellow banana for me."
 message = Message(test_input)
 
 tk = WhitespaceTokenizer()
-tk.process(message)
-tokens = message.get(TOKENS_NAMES[TEXT])
+tokens = tk.tokenize(message, attribute=TEXT)
 logger.info('Whitespace: {}'.format([t.text for t in tokens]))
-#
-# tk = SpacyTokenizer()
-# # message.set(SPACY_DOCS[TEXT], spacy_nlp(test_input))
-# tokens = tk.tokenize(message, attribute=TEXT)
-# print([t.text for t in tokens])
+
+tk = SpacyTokenizer()
+spacy_nlp_config = {"name": "SpacyNLP"}
+blank_config = RasaNLUModelConfig({"language": "en", "pipeline": []})
+spacy_nlp = ComponentBuilder().create_component(spacy_nlp_config, blank_config).nlp
+message.set(SPACY_DOCS[TEXT], spacy_nlp(test_input))
+tokens = tk.tokenize(message, attribute=TEXT)
+logger.info('SpaCy: {}'.format([t.text for t in tokens]))
 
 tk = MitieTokenizer()
-tk.process(message)
 tokens = tk.tokenize(message, attribute=TEXT)
 logger.info('Mitie: {}'.format([t.text for t in tokens]))
 
@@ -55,9 +56,8 @@ tk = ConveRTTokenizer()
 tokens = tk.tokenize(message, attribute=TEXT)
 logger.info('ConveRT: {}'.format([t.text for t in tokens]))
 
+tk = LanguageModelTokenizer()
 transformers_nlp = HFTransformersNLP({"model_name": "bert"})
-lm_tokenizer = LanguageModelTokenizer()
-message = Message.build(text=test_input)
 transformers_nlp.process(message)
-tokens = lm_tokenizer.tokenize(message, TEXT)
+tokens = tk.tokenize(message, attribute=TEXT)
 logger.info('BERT: {}'.format([t.text for t in tokens]))
