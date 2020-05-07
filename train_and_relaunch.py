2fb3c6f347a003ac38a7b10a3c261d540edebe34
Lars Væhrens <lvahre16@student.aau.dk>
2020-05-07 17:01:41 +0200

    Added timer for training rasa core and nlu model

--- train_and_relaunch.py
+++ train_and_relaunch.py
@@ -4,28 +4,32 @@ import subprocess
 import sys
 
 from rasa.train import train
+from utils.timing import CodeTimer
 
 logger = logging.getLogger(__name__)
 
 domain_file = './domain.yml'
 config_file = './config.yml'
-nlu_data = './data/nlu/'
+nlu_data = './data/'
 output_path = './models/'
 model_name = 'fruits_model'
 
+
 def train_rasa():
-    train(
-        domain=domain_file,
-        config=config_file,
-        training_files=nlu_data, 
-        output=output_path,
-        #force_training=True,
-        fixed_model_name=model_name)
+    with CodeTimer() as timer:
+        train(
+            domain=domain_file,
+            config=config_file,
+            training_files=nlu_data, 
+            output=output_path,
+            #force_training=True,
+            fixed_model_name=model_name)
+    time = timer.took * 0.001
+    print('Training time took: {} sec.'.format(time))
 
     try:
         test = platform.uname()
         if test[0] == "Linux":
-            subprocess.call(['./nlp_magic/bin/rasa_actions.sh'])
             subprocess.call(['./nlp_magic/bin/rasa_bot.sh'])
         else:
             return []
