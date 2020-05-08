10dca726aeb198bcfad0ff0c8649123c5ae6a6a1
glhr <grenblot@gmail.com>
2020-05-08 15:46:42 +0200

    removed unused components from pipelines and added evaluation script

---
+++ eval_nlu.sh
@@ -0,0 +1,1 @@
+rasa test nlu --config pipelines/pretrained_embeddings_spacy.yml pipelines/custom.yml pipelines/supervised_embeddings.yml --nlu data/nlu/ --runs 3 --percentages 0 10 20 30 40 50 60 70 80
