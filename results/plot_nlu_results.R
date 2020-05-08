e6fd530c7fe7afd2b458965d443a91b05d193137
glhr <grenblot@gmail.com>
2020-05-08 21:42:00 +0200

    updated R script for plotting entity accuracy

--- results/plot_nlu_results.R
+++ results/plot_nlu_results.R
@@ -21,3 +21,30 @@ intents_plot + geom_col(position="dodge",
                    y="F1 score (average across 2 runs)",
                    fill="RASA pipeline") +
                 theme(legend.position = "top")
+
+#############################################
+
+entity_results <- read.csv("~/catkin_ws/src/lh7-nlp/nlp_magic/src/rasa_project/results/entity_results.csv")
+
+# for a split of 10%, plotting average F1 score across runs for each entity
+split_10.f1 <- subset(entity_results, (split==10 & X.1=='f1-score'))
+
+split_10.f1 = subset(split_10.f1, select = c(X, object_name, object_color, undefined_object, placement, pipeline))
+
+split_10.f1.avg = aggregate(x=split_10.f1,
+                            by = list(pipeline = split_10.f1$pipeline),
+                            FUN=mean)
+
+split_10.f1.avg = subset(split_10.f1.avg, select = c(object_name, object_color, undefined_object, placement, pipeline))
+data.split_10.f1.avg <- melt(split_10.f1.avg,
+                             id.vars=c("pipeline"),
+                             measure.vars = c("object_name", "object_color", "undefined_object", "placement"))
+
+entitys_plot <- ggplot(data.split_10.f1.avg,aes(variable,value, fill=pipeline))
+entitys_plot + geom_col(position="dodge",
+                        width=0.7) +
+  labs(x="Entity",
+       y="F1 score (average across 2 runs)",
+       fill="RASA pipeline") +
+  theme(legend.position = "top")
+
