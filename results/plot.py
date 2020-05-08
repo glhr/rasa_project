47cd388acda20ffca2fa25af6f40aa29984f4754
glhr <grenblot@gmail.com>
2020-05-08 21:38:48 +0200

    updated plotting to generate CSV for entity extraction

--- results/plot.py
+++ results/plot.py
@@ -3,8 +3,10 @@ import pandas as pd
 dfs_pipelines = []
 percents = range(0,20,10)
 runs = range(1,3)
+pipelines = ['custom', 'pretrained_embeddings_spacy', 'supervised_embeddings']
 
-for pipeline in ['custom', 'pretrained_embeddings_spacy', 'supervised_embeddings']:
+# generate CSV for intent classification results
+for pipeline in pipelines:
     for percent in percents:
         dfs_runs = []
         for run in runs:
@@ -26,3 +28,35 @@ for pipeline in ['custom', 'pretrained_embeddings_spacy', 'supervised_embeddings
 dfs_pipelines = pd.concat(dfs_pipelines)
 print(dfs_pipelines)
 dfs_pipelines.to_csv('intent_results.csv')
+
+dfs_pipelines = []
+extractors = {
+    'custom': 'DIETClassifier',
+    'pretrained_embeddings_spacy': 'CRFEntityExtractor',
+    'supervised_embeddings': 'CRFEntityExtractor'
+}
+
+# generate CSV for entity extraction results
+for pipeline in pipelines:
+    for percent in percents:
+        dfs_runs = []
+        for run in runs:
+            extractor = extractors[pipeline]
+            file = 'run_{}/{}%_exclusion/{}_report/{}_report.json'.format(
+                run,
+                percent,
+                pipeline,
+                extractor
+            )
+            df = pd.read_json(file)
+            df = df.drop(['support'])
+            print(df)
+            dfs_runs.append(df)
+        df_avg = pd.concat(dfs_runs, keys=runs)
+        df_avg['pipeline'] = pipeline
+        df_avg['split'] = percent
+        dfs_pipelines.append(df_avg)
+
+dfs_pipelines = pd.concat(dfs_pipelines)
+print(dfs_pipelines)
+dfs_pipelines.to_csv('entity_results.csv')
