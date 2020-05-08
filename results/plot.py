e45e2a8af6403e5583e55631fdfa75424800cbc9
glhr <grenblot@gmail.com>
2020-05-08 17:10:53 +0200

    updated plotting to save intent reports to a single CSV

--- results/plot.py
+++ results/plot.py
@@ -3,21 +3,23 @@ import pandas as pd
 dfs_pipelines = []
 
 for pipeline in ['custom', 'pretrained_embeddings_spacy', 'supervised_embeddings']:
-    dfs_runs = []
-    for run in range(1,3):
-        file = 'run_{}/0%_exclusion/{}_report/intent_report.json'.format(
-            run,
-            pipeline
-        )
-        df = pd.read_json(file)
-        df = df.drop(['confused_with', 'support'])
-        # print(df)
-        dfs_runs.append(df)
-    df_avg = pd.concat(dfs_runs).groupby(level=0).mean()
-    df_avg['pipeline'] = pipeline
-    df_avg['split'] = 0
-
-    dfs_pipelines.append(df_avg)
+    for percent in range(0,20,10):
+        for run in range(1,3):
+            dfs_runs = []
+            file = 'run_{}/{}%_exclusion/{}_report/intent_report.json'.format(
+                run,
+                percent,
+                pipeline
+            )
+            df = pd.read_json(file)
+            df = df.drop(['confused_with', 'support'])
+            # print(df)
+            dfs_runs.append(df)
+        df_avg = pd.concat(dfs_runs).groupby(level=0).mean()
+        df_avg['pipeline'] = pipeline
+        df_avg['split'] = percent
+        dfs_pipelines.append(df_avg)
 
 dfs_pipelines = pd.concat(dfs_pipelines)
 print(dfs_pipelines)
+dfs_pipelines.to_csv('intent_results.csv')
