3d76e3b96242af83235f9c0cb90085a7ae2cf344
Lars <lvahre16@student.aau.dk>
2020-05-13 08:42:22 +0200

    Updated pipelines with number of epochs and evaluation script

--- eval_nlu.sh
+++ eval_nlu.sh
@@ -1,1 +1,1 @@
-rasa test nlu --config pipelines/pretrained_embeddings_spacy.yml pipelines/custom.yml pipelines/supervised_embeddings.yml --nlu data/nlu/ --runs 3 --percentages 0 10 20 30 40 50 60 70 80
+rasa test nlu --config pipelines/recommended.yml pipelines/pretrained_embeddings_spacy.yml pipelines/custom.yml pipelines/supervised_embeddings.yml pipelines/mitie.yml --nlu data/nlu/ --runs 3 --percentages 0 10 20 30 40 50 60 70 80
