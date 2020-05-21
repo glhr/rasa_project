9fdff92b19955567ec9d8e1b4fd0501d03fc88b3
Lars <lvahre16@student.aau.dk>
2020-05-21 16:43:44 +0200

    Added all evaluations across pipelines (without and with user test data)

---
+++ results/plot_nlu_results.R
@@ -0,0 +1,58 @@
+intent_results <- read.csv("~/catkin_ws/src/lh7-nlp/nlp_magic/src/rasa_project/results/intent_results.csv")
+
+# for a split of 10%, plotting average F1 score across runs for each intent
+split_10.f1 <- subset(intent_results, (split==10 & X.1=='f1-score'))
+
+split_10.f1 = subset(split_10.f1, select = c(X, greetings, bye, none, affirmative, deny, show, find, pick.up, move, clarify, pipeline))
+
+split_10.f1.avg = aggregate(x=split_10.f1,
+                            by = list(pipeline = split_10.f1$pipeline),
+                            FUN=mean)
+
+split_10.f1.avg = subset(split_10.f1.avg, select = c(greetings, bye, none, affirmative, deny, show, find, pick.up, move, clarify, pipeline))
+data.split_10.f1.avg <- melt(split_10.f1.avg,
+                             id.vars=c("pipeline"),
+                             measure.vars = c("greetings", "bye", "none", "affirmative", "deny", "show", "find", "pick.up", "move", "clarify"))
+
+intents_plot <- ggplot(data.split_10.f1.avg,
+                       aes(x=reorder(variable, -value),
+                           y=value,
+                           fill=pipeline))
+intents_plot + geom_col(position="dodge",
+                            width=0.6) +
+                labs(x="Intent",
+                   y="F1 score (average across 3 runs)",
+                   fill="RASA pipeline") +
+                theme(legend.position = "right")
+ggsave("intent-eval-0.1.png",width = 10, height=3)
+
+#############################################
+
+entity_results <- read.csv("~/catkin_ws/src/lh7-nlp/nlp_magic/src/rasa_project/results/entity_results.csv")
+
+# for a split of 10%, plotting average F1 score across runs for each entity
+split_10.f1 <- subset(entity_results, (split==10 & X.1=='f1-score'))
+
+split_10.f1 = subset(split_10.f1, select = c(X, object_name, object_color, undefined_object, placement, pipeline))
+
+split_10.f1.avg = aggregate(x=split_10.f1,
+                            by = list(pipeline = split_10.f1$pipeline),
+                            FUN=mean)
+
+split_10.f1.avg = subset(split_10.f1.avg, select = c(object_name, object_color, undefined_object, placement, pipeline))
+data.split_10.f1.avg <- melt(split_10.f1.avg,
+                             id.vars=c("pipeline"),
+                             measure.vars = c("object_name", "object_color", "undefined_object", "placement"))
+
+entitys_plot <- ggplot(data.split_10.f1.avg,
+                       aes(reorder(variable, -value),
+                           value,
+                           fill=pipeline))
+entitys_plot + geom_col(position="dodge",
+                        width=0.5) +
+  labs(x="Entity",
+       y="F1 score (average across 3 runs)",
+       fill="RASA pipeline") +
+  theme(legend.position = "right")
+ggsave("entity-eval-0.1.png",width = 8, height=3)
+
